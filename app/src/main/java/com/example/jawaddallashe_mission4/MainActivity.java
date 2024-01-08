b83059e9ca4af94a57660223c60efe19168a6964
DESKTOP-FIA3LA0 <jawaddl2007@Gmail.com>
2024-01-08 23:47:17 +0200

    jawaddallashe_mission4

--- app/src/main/java/com/example/jawaddallashe_mission4/MainActivity.java
+++ app/src/main/java/com/example/jawaddallashe_mission4/MainActivity.java
@@ -1,14 +1,43 @@
 package com.example.jawaddallashe_mission4;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    private Button btn;
+
+    int count = 0;
+    int i;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //abc
+        btn = findViewById(R.id.button);
+        btn.setBackgroundColor(Color.RED);
+        btn.setTextSize(30);
+    }
+
+
+    public void go(View view) {
+
+        count++;
+        String str = count + " ";
+        btn.setText(str);
+        btn.setText("This is a click number:" + count);
+        btn.setTextSize(30);
+        btn.setTextColor(Color.BLACK);
+        if (count % 7 == 0) {
+            btn.setText("BOOM !");
+
+        }
+
     }
 }
